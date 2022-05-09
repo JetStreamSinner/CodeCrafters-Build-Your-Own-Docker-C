360d94b5bb3975f91cc2bf1388bc79435f88ce8b
Jetstream <bigalex934@gmail.com>
2022-05-09 16:40:37 +0300

    Implement stage 3

--- app/main.c
+++ app/main.c
@@ -3,24 +3,25 @@
 #include <unistd.h>
 
 #define FORKING_ERROR -1
-#define FORKING_SUCCESS 0
+#define INHERITOR_PID 0
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
-
     char * command = argv[3];
-
     int child_pid = fork();
 
-    if (child_pid == FORKING_ERROR) {
-        return 1;
-    }
-
-    if (child_pid == FORKING_SUCCESS) {
-        execv(command, &argv[3]);
-    } else {
-        wait(NULL);
+    switch (child_pid) {
+        case FORKING_ERROR:
+            return 1;
+        case INHERITOR_PID:
+            execv(command, &argv[3]);
+            break;
+        default: {
+            int child_status;
+            wait(&child_status);
+            return WEXITSTATUS(child_status);
+        }
     }
 
     return 0;
