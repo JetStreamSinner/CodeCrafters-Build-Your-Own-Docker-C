b4f82b709ad5ddcf081bca40d6c81c598b6369ab
Jetstream <bigalex934@gmail.com>
2022-05-10 18:43:42 +0300

    Implement filesystem isolation

--- app/main.c
+++ app/main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-#define FORKING_ERROR -1
-#define INHERITOR_PID 0
+#define FORK_ERROR_CODE -1
+#define FORKED_SUCCESS 0
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
     char * command = argv[3];
     int child_pid = fork();
 
-    switch (child_pid) {
-        case FORKING_ERROR:
-            return 1;
-        case INHERITOR_PID:
-            execv(command, &argv[3]);
-            break;
-        default: {
-            int child_status;
-            wait(&child_status);
-            return WEXITSTATUS(child_status);
-        }
+    if (child_pid == FORK_ERROR_CODE)
+        return 1;
+
+    if (child_pid == FORKED_SUCCESS) {
+        char temporaryDirPath[] = "./temp";
+        int temporaryDirPermissions = 700;
+
+        mkdir(temporaryDirPath, temporaryDirPermissions);
+        chroot(temporaryDirPath);
+
+        execv(command, &argv[3]);
+    } else {
+        int child_status;
+        wait(&child_status);
+        return WEXITSTATUS(child_status);
     }
 
     return 0;
